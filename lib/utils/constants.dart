9376e317114cfef0aee7b5adce60c408b9b381ec
im2rnado <yassin.bedier@gmail.com>
2022-12-25 22:37:24 +0200

    Fix demo version

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -31,7 +31,7 @@ const String notificationChannelKey = "basic_channel";
 
 //Set demo verion this when upload this code to codecanyon
 //Is demo version
-//const bool isDemoVersion = false;
+const bool isDemoVersion = false;
 
 String getExamStatusTypeKey(String examStatus) {
   if (examStatus == "0") {
